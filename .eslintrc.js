16762bf99a45ed052fceec3b252faa0c01df773e
Nathan Graule <solarliner@gmail.com>
2019-03-21 00:20:11 +0100

    Finalize eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     es6: true,
     node: true
   },
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier", "prettier/@typescript-eslint"],
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint", "prettier"],
   globals: {
@@ -17,6 +17,6 @@ module.exports = {
     project: "./tsconfig.json"
   },
   rules: {
-    "prettier/prettier": true
+    "prettier/prettier": 1
   }
 };
