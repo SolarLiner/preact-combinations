cd8201bd0c0fd7f8f0d79f9b12e50ee3c6cd887e
Nathan Graule <solarliner@gmail.com>
2019-03-23 02:14:10 +0100

    Fix table layout in table combinations component

--- src/components/combinations/table.tsx
+++ src/components/combinations/table.tsx
@@ -18,9 +18,12 @@ export default function CombinationsTable<T, A extends keyof T, B extends keyof 
   props: RenderableProps<CTableProps<T, A, B>>
 ) {
   return (
-    <table style={props.style}>
+    <table style={Object.assign({}, props.style, { border: "1px black" })}>
       <tbody>
         <tr>
+          <th>
+            {props.one.name}\{props.two.name}
+          </th>
           {props.one.values.map(v => (
             <th>{v.toString()}</th>
           ))}
@@ -28,9 +31,9 @@ export default function CombinationsTable<T, A extends keyof T, B extends keyof 
         {props.two.values.map(vt => (
           <tr>
             <th>{vt.toString()}</th>
-            {props.one.values.forEach(vo =>
-              h(props.component, { [props.one.name]: vo, [props.two.name]: vt } as any, props.children)
-            )}
+            {props.one.values.map(vo => (
+              <td>{h(props.component, { [props.one.name]: vo, [props.two.name]: vt } as any, props.children)}</td>
+            ))}
           </tr>
         ))}
       </tbody>
